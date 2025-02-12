42a42602146a723b8fa11e706961f8ef91dbe495
fatima-amani <1rn21ai048.fatimaamani@gmail.com>
2025-02-12 23:51:15 +0530

    added initial files

---
+++ src/test/java/com/fatima/metro_service/MetroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatima.metro_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
